9fe1b49c6b60b72dd411a9db5a384b26b457b8c2
Hemanta Pradhan <your-email@example.com>
2024-09-20 22:05:37 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let a ="Welcome to Hemanta Pradhan's Official Website – Your Gateway to Inspiration and Insight.";
+
+if (a === "Welcome to Hemanta Pradhan's Official Website – Your Gateway to Inspiration and Insight.") {
+    alert(a);
+}
